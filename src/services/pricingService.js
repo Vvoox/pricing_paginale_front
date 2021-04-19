2a48a9934b75bafec73efc2ddffe832327226b48
Khalil <daoulat.khalil@gmail.com>
2021-04-19 16:00:54 +0100

    Get params from URL

--- src/services/pricingService.js
+++ src/services/pricingService.js
@@ -2,12 +2,22 @@ import axios from 'axios';
 // import interceptor from '../utils/interceptor';
 
 const CHARGE_API = "http://localhost:3001/master-handler/apps/orderify/api/add-charges/trial";
+const CHARGE_API1 = "http://localhost:3001/master-handler/apps/orderify";
 const RETURN_API = "http://orderify.paginale.com/";
+const params = window.location.search.split("?")[1].split("&");
+const token = params[0].split("=")[1]
+const shop = params[1].split("=")[1]
+console.log(shop)
+console.log(token)
 
 class PricingService {
 
     addBasicCharge_month(){
+
         const charges ={
+            plan:"basic_month",
+            shop:shop,
+            token:token,
             recurring_application_charge: {
                 name: "Orderify",
                 price: 6.99,
@@ -20,7 +30,10 @@ class PricingService {
     }
     addBasicCharge_yearly(){
             const charges ={
-            recurring_application_charge: {
+                plan:"basic_year",
+                shop:shop,
+                token:token,
+                recurring_application_charge: {
                 name: "Orderify",
                 price: 79.99,
                 return_url: RETURN_API,
@@ -33,6 +46,9 @@ class PricingService {
     //---------------------------------------- Creative Plan --------------------------------------------------------
     addCreativeCharge_monthly(){
         const charges ={
+            plan:"creative_month",
+            shop:shop,
+            token:token,
             recurring_application_charge: {
                 name: "Orderify",
                 price: 149.99,
@@ -45,6 +61,9 @@ class PricingService {
     }
     addCreativeCharge_yearly(){
         const charges ={
+            plan:"creative_year",
+            shop:shop,
+            token:token,
             recurring_application_charge: {
                 name: "Orderify",
                 price: 149.99,
@@ -58,6 +77,9 @@ class PricingService {
     //---------------------------------------- Premium Plan --------------------------------------------------------
     addPremiumCharge_monthly(){
         const charges ={
+            plan:"premium_month",
+            shop:shop,
+            token:token,
             recurring_application_charge: {
                 name: "Orderify",
                 price: 13.99,
@@ -70,6 +92,9 @@ class PricingService {
     }
     addPremiumCharge_yearly(){
         const charges ={
+            plan:"premium_year",
+            shop:shop,
+            token:token,
             recurring_application_charge: {
                 name: "Orderify",
                 price: 165.99,
