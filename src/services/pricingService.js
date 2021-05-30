33500c4cb42856530df10666c06a988c48b6e04a
Khalil <daoulat.khalil@gmail.com>
2021-05-30 02:16:17 +0000

    *Fix errors

--- src/services/pricingService.js
+++ src/services/pricingService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // import interceptor from '../utils/interceptor';
 
-const CHARGE_API = "https://gateway.paginale.com/master-handler/apps/codify/api/add-charges/trial";
+const CHARGE_API = "https://gateway.paginale.com/hello";
 // const CHARGE_API = "gateway.paginale.com/hello";
 // const SERVER_API = "http://gateway.paginale.com/master-handler/apps/codify/api/add-charges/trial";
 // const CHARGE_API1 = "http://localhost:3002/master-handler/apps/codify";
