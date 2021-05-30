9f960bfdef41c7cef0a450071432011849d57d53
Khalil <daoulat.khalil@gmail.com>
2021-05-30 00:41:05 +0000

    *Fix errors

--- src/services/pricingService.js
+++ src/services/pricingService.js
@@ -35,7 +35,7 @@ class PricingService {
         };
         return fetch(CHARGE_API, {
             method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, cors, *same-origin
+            mode: "no-cors", // no-cors, cors, *same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
             credentials: "same-origin", // include, *same-origin, omit
             headers: {
